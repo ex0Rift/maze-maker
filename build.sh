eec80d6fe03db7874d7ad27fe8f477506ca8192e
Nate <daviesnathanael45@gmail.com>
2026-03-17 11:13:03 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Compiling..."
+g++ \
+    src/main.cpp \
+    -I include \
+    -o build \
+    -lraylib -lm -ldl -lGL -pthread
+if [ $? -ne 0 ]; then
+    echo "Compile failed! exiting..."
+    exit 1
+fi
+./build
---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "raylib.h"
+
+int main(){
+    //==
+    // Initialisation
+    //==
+    InitWindow(1000,600,"3d-raylib");
+    SetTargetFPS(60);
+
+    //camera for 3D
+    Camera camera = {0};
+    camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+
+    while (!WindowShouldClose()){
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube({0.0f, 1.0f, 0.0f},2.0f,2.0f,2.0f,RED);
+
+
+                DrawGrid(15,1.0f);
+            EndMode3D();
+
+            DrawFPS(10,10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
