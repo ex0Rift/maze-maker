26ea2e5246674fee43031566aba6ed47c922c86e
Nate <daviesnathanael45@gmail.com>
2026-03-17 11:24:47 +0000

    camera moves like a player and basic plane

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,28 @@ int main(){
     InitWindow(1000,600,"3d-raylib");
     SetTargetFPS(60);
 
+    DisableCursor();
+
     //camera for 3D
     Camera camera = {0};
-    camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+    camera.position = (Vector3){10.0f, 2.0f, 10.0f};
     camera.target = (Vector3){0.0f, 0.0f, 0.0f};
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-
+    //==
+    // Mainloop
+    //==
     while (!WindowShouldClose()){
+        //--
+        //Logic
+        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
 
+
+
+        //--
+        //Drawing
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -27,7 +38,7 @@ int main(){
                 DrawCube({0.0f, 1.0f, 0.0f},2.0f,2.0f,2.0f,RED);
 
 
-                DrawGrid(15,1.0f);
+                DrawGrid(30,1.0f);
             EndMode3D();
 
             DrawFPS(10,10);
